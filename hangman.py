a2f64320cd58772c082e363be12b0e6f6d861373
JochemJetLearn <jochemdebree0@gmail.com>
2025-10-07 17:54:30 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,18 +4,22 @@ words = ["open", "enter", "hydroneer", "programming", "netherlands", "super", "h
 
 word = list(random.choice(words))
 guessed = []
+stats = [0,0]
 for i in word:
     guessed.append("_")
 while True:
     print(guessed)
     letter = input("What letter do you choose ")
     if letter in word:
+        stats[0] += 1
         for i in range(len(word)):
             if word[i] == letter:
                 guessed[i] = letter
     else:
+        stats[1] += 1
         print("Wrong guess, try again")
     if word == guessed:
         print("Congrats, you win")
         break
 print(f"you win, stats:\n -correct: {stats[0]}\n -incorrect: {stats[1]}")
+
