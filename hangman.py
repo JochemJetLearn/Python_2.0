aa22340a0afa917241486ab44590b99a94a02633
JochemJetLearn <jochemdebree0@gmail.com>
2025-10-07 17:34:25 +0200

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,20 @@
+import random
+
+words = ["open", "enter", "hydroneer", "programming", "netherlands", "super", "high", "snake", "python", "java", "coding", "script"]
+
+word = ["h","i","g","h"] # list(random.choice(words))
+guessed = []
+for i in word:
+    guessed.append("_")
+while True:
+    print(guessed)
+    letter = input("What letter do you choose ")
+    if letter in word:
+        for i in range(len(word)):
+            if word[i] == letter:
+                guessed[i] = letter
+    else:
+        print("Wrong guess, try again")
+    if word == guessed:
+        print("Congrats, you win")
+        break
