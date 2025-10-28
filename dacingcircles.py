8a48513412daf99fe09306f3cc90cf403c01987d
JochemJetLearn <jochemdebree0@gmail.com>
2025-10-28 17:34:10 +0100

    Add files via upload

--- dacingcircles.py
+++ dacingcircles.py
@@ -1,6 +1,14 @@
 import turtle, random, time
 
+while True:
+    try:
+        rounds = int(input("How many rounds do you want? "))
+        break
+    except:
+        pass
+
 screen = turtle.Screen()
+screen.title("Shapes animates")
 pen = turtle.Turtle()
 screen.bgcolor("black")
 pen.speed(0)
@@ -8,6 +16,7 @@ colours = ["red", "blue", "cyan", "pink", "orange", "purple", "green"]
 
 def circle(speed):
     color = random.choice(colours)
+    pen.right(random.randint(0, 360))
     pen.color(color)
     pen.speed(speed)
     pen.up()
@@ -19,12 +28,112 @@ def circle(speed):
     pen.end_fill()
     pen.hideturtle()
 
+def square(speed):
+    color = random.choice(colours)
+    pen.right(random.randint(0, 360))
+    pen.color(color)
+    pen.speed(speed)
+    pen.up()
+    pen.goto(random.randint(-150, 150), random.randint(-150, 150))
+    pen.down()
+    pen.fillcolor(color)
+    pen.begin_fill()
+    for i in range(4):
+        pen.forward(100)
+        pen.right(90)
+    pen.end_fill()
+    pen.hideturtle()
+
+def triangle(speed):
+    color = random.choice(colours)
+    pen.right(random.randint(0, 360))
+    pen.color(color)
+    pen.speed(speed)
+    pen.up()
+    pen.goto(random.randint(-150, 150), random.randint(-150, 150))
+    pen.down()
+    pen.fillcolor(color)
+    pen.begin_fill()
+    for i in range(3):
+        pen.forward(100)
+        pen.right(120)
+    pen.end_fill()
+    pen.hideturtle()
+
+def pentagon(speed):
+    color = random.choice(colours)
+    pen.right(random.randint(0, 360))
+    pen.color(color)
+    pen.speed(speed)
+    pen.up()
+    pen.goto(random.randint(-150, 150), random.randint(-150, 150))
+    pen.down()
+    pen.fillcolor(color)
+    pen.begin_fill()
+    for i in range(5):
+        pen.forward(100)
+        pen.right(72)
+    pen.end_fill()
+    pen.hideturtle()
+
+def hexagon(speed):
+    color = random.choice(colours)
+    pen.right(random.randint(0, 360))
+    pen.color(color)
+    pen.speed(speed)
+    pen.up()
+    pen.goto(random.randint(-150, 150), random.randint(-150, 150))
+    pen.down()
+    pen.fillcolor(color)
+    pen.begin_fill()
+    for i in range(6):
+        pen.forward(100)
+        pen.right(60)
+    pen.end_fill()
+    pen.hideturtle()
+
+def septagon(speed):
+    color = random.choice(colours)
+    pen.right(random.randint(0, 360))
+    pen.color(color)
+    pen.speed(speed)
+    pen.up()
+    pen.goto(random.randint(-150, 150), random.randint(-150, 150))
+    pen.down()
+    pen.fillcolor(color)
+    pen.begin_fill()
+    for i in range(7):
+        pen.forward(100)
+        pen.right(360/7)
+    pen.end_fill()
+    pen.hideturtle()
+
+def octagon(speed):
+    color = random.choice(colours)
+    pen.right(random.randint(0, 360))
+    pen.color(color)
+    pen.speed(speed)
+    pen.up()
+    pen.goto(random.randint(-150, 150), random.randint(-150, 150))
+    pen.down()
+    pen.fillcolor(color)
+    pen.begin_fill()
+    for i in range(8):
+        pen.forward(100)
+        pen.right(45)
+    pen.end_fill()
+    pen.hideturtle()
+
+shapes = [circle, square, triangle, pentagon, hexagon, septagon, octagon]
+
 starttime = time.time()
 
-for i in range(50):
+for i in range(rounds):
     pen.clear()
-    circle(i*10)
+    print(f"Shape {i}/{rounds} at {round(time.time() - starttime, 2)}s")
+    random.choice(shapes)(i*10)
     time.sleep(2/(i+1))
+print(f"Shape {rounds}/{rounds} - Animation complete")
 
 endtime = time.time()
 
